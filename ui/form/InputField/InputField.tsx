5cc4eb7b6fd257773a2e82dd62a0668c77f5fc75
Денис Крылов <krydenk@mail.ru>
2020-02-14 18:01:01 +0700

    fix onChange to make compatible with React Native

--- ui/form/InputField/InputField.tsx
+++ ui/form/InputField/InputField.tsx
@@ -63,7 +63,7 @@ export default class InputField extends React.PureComponent<IInputFieldProps & I
                     type: this.props.type,
                     name: this.props.input.name,
                     value: this.props.input.value || "",
-                    onChange: e => this.props.input.onChange(e.target.value),
+                    onChange: value => this.props.input.onChange(value),
                     placeholder: this.props.placeholder,
                     disabled: this.props.disabled,
                     ...this.props.inputProps

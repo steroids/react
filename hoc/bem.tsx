93e5fdd39a7edbb46845701d6df5eadbba3e5a84
Денис Крылов <krydenk@mail.ru>
2020-02-27 10:40:43 +0700

    Importing elements styles with bem hoc (for React Native)

--- hoc/bem.tsx
+++ hoc/bem.tsx
@@ -14,16 +14,23 @@ export interface IBemHocPrivateProps {
     components: IComponentsHocOutput,
 }
 
-export default (namespace: string): any => WrappedComponent =>
+export default (namespace: string, styles = null): any => WrappedComponent =>
     components()(
         class BemHoc extends React.PureComponent<IBemHocInput & IBemHocPrivateProps> {
             static WrappedComponent = WrappedComponent;
 
             render() {
                 const props = {} as IBemHocOutput;
+
                 if (namespace) {
                     props.bem = this.props.components.html.bem(namespace);
                 }
+
+                // Only for React Native
+                if (styles && this.props.components.html.addStyles) {
+                    this.props.components.html.addStyles(styles);
+                }
+
                 return (
                     <WrappedComponent
                         {...this.props}

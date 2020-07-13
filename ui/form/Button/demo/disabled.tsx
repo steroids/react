59de7f7c54f8e776fc074f79cc3a6c3e3a28417c
gosha <nitrogosha2014@mail.ru>
2020-07-13 17:21:08 +0700

    add static link on github for button examples

--- ui/form/Button/demo/disabled.tsx
+++ ui/form/Button/demo/disabled.tsx
@@ -11,6 +11,14 @@ export default class extends React.PureComponent {
     render() {
         return (
             <>
+                <div>
+                    <a
+                        target={'_blank'}
+                        href={'https://github.com/steroids/react/blob/master/ui/form/Button/demo/disabled.tsx'}>
+                        Github
+                    </a>
+                </div>
+                <br/>
                 <Button disabled>
                     Disabled
                 </Button>
--- ui/form/Button/demo/link.tsx
+++ ui/form/Button/demo/link.tsx
@@ -11,6 +11,14 @@ export default class extends React.PureComponent {
     render() {
         return (
             <>
+                <div>
+                    <a
+                        target={'_blank'}
+                        href={'https://github.com/steroids/react/blob/master/ui/form/Button/demo/link.tsx'}>
+                        Github
+                    </a>
+                </div>
+                <br/>
                 <Button
                     link
                     url='https://google.ru'

02d17b106a7501af82cea87017960a5cd9b0471a
krydenk93 <krydenk@gmail.com>
2020-08-05 16:12:17 +0700

    Rolling back "Disallow to send cookies on mobile platform"

--- components/HttpComponent.tsx
+++ components/HttpComponent.tsx
@@ -29,7 +29,7 @@ export default class HttpComponent {
 
     async getAxiosConfig() {
         const config = {
-            withCredentials: process.env.PLATFORM !== 'mobile',
+            withCredentials: true,
             headers: {
                 // Add XMLHttpRequest header for detect ajax requests
                 'X-Requested-With': 'XMLHttpRequest',

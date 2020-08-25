d8ad259655ebbc7be58bea097742d13326eb426d
kahegal <kahegal89@gmail.com>
2020-08-25 18:09:56 +0700

    improve NotificationsCore

--- actions/notifications.ts
+++ actions/notifications.ts
@@ -2,16 +2,19 @@ let ID_COUNTER = 0;
 export const NOTIFICATIONS_SHOW = 'NOTIFICATIONS_SHOW';
 export const NOTIFICATIONS_CLOSING = 'NOTIFICATIONS_CLOSING';
 export const NOTIFICATIONS_CLOSE = 'NOTIFICATIONS_CLOSE';
-export const showNotification = (
-  message,
-  level = 'warning',
-  timeOut = 10000
-) => dispatch => {
+export const showNotification =
+  ({
+    message,
+    level = 'warning',
+    position = 'top-right',
+    timeOut = 8000,
+  }) => dispatch => {
   const id = ++ID_COUNTER;
-  dispatch({ type: NOTIFICATIONS_SHOW, id, message, level });
-  setTimeout(() => dispatch({ type: NOTIFICATIONS_CLOSE, id }), timeOut);
+  dispatch({ type: NOTIFICATIONS_SHOW, id, message, level, position});
+  if(timeOut > 0){
+    setTimeout(() => dispatch(closeNotification(id)), timeOut);
+  }
 };
-export const setClosing = (id = null) => ({ type: NOTIFICATIONS_CLOSING, id });
 export const closeNotification = (id = null) => ({
   type: NOTIFICATIONS_CLOSE,
   id
@@ -20,5 +23,5 @@ export const setFlashes = flashes =>
   Object.keys(flashes).map(level => {
     return []
       .concat(flashes[level] || [])
-      .map(message => showNotification(message, level));
+      .map(message => showNotification({message, level}));
   });

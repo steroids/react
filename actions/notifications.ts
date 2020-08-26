cf0135acc553caf6a2dd359b1983447132070916
kahegal <kahegal89@gmail.com>
2020-08-26 17:47:22 +0700

    notifications - add interface to action, modify demo-page

--- actions/notifications.ts
+++ actions/notifications.ts
@@ -2,26 +2,38 @@ let ID_COUNTER = 0;
 export const NOTIFICATIONS_SHOW = 'NOTIFICATIONS_SHOW';
 export const NOTIFICATIONS_CLOSING = 'NOTIFICATIONS_CLOSING';
 export const NOTIFICATIONS_CLOSE = 'NOTIFICATIONS_CLOSE';
-export const showNotification =
-  ({
-    message,
-    level = 'warning',
-    position = 'top-right',
-    timeOut = 8000,
-  }) => dispatch => {
-  const id = ++ID_COUNTER;
-  dispatch({ type: NOTIFICATIONS_SHOW, id, message, level, position});
-  if(timeOut > 0){
-    setTimeout(() => dispatch(closeNotification(id)), timeOut);
-  }
+
+export interface IShowNotificationParameters {
+    level?: ColorName;
+    position?: 'top-right' | 'bottom-right' | 'top-left' | 'bottom-left' | string;
+    timeOut?: number;
+}
+
+const showNotificationDefaults: IShowNotificationParameters = {
+    level: 'success',
+    position: 'top-right',
+    timeOut: 10000,
 };
+
+export const showNotification = (message: string, params: IShowNotificationParameters = {}) => dispatch => {
+    const {level, position, timeOut} = {...showNotificationDefaults, ...params};
+    const id = ++ID_COUNTER;
+
+    dispatch({ type: NOTIFICATIONS_SHOW, id, message, level, position});
+
+    if(timeOut > 0){
+        setTimeout(() => dispatch(closeNotification(id)), timeOut);
+    }
+};
+
 export const closeNotification = (id = null) => ({
   type: NOTIFICATIONS_CLOSE,
   id
 });
+
 export const setFlashes = flashes =>
-  Object.keys(flashes).map(level => {
+  Object.keys(flashes).map((level: string) => {
     return []
       .concat(flashes[level] || [])
-      .map(message => showNotification({message, level}));
+      .map(message => showNotification(message, {level}));
   });

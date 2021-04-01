717fab2a4488ed09e4f1f16e77df889860ab335a
nlichodeevskiy <nicklihorus4@gmail.com>
2021-04-01 15:16:49 +0700

    add useUniqueId hook, upd views

---
+++ hooks/useUniqueId.ts
@@ -0,0 +1,10 @@
+import {useRef} from 'react';
+import _uniqueId from 'lodash-es/uniqueId';
+
+export default function useUniqueId(prefix: string) {
+    const idRef = useRef();
+    if (!idRef.current) {
+        idRef.current = _uniqueId(prefix);
+    }
+    return idRef.current;
+}

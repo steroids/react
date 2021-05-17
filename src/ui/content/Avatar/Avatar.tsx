8dbbce7de8ac71c89f67e10bc687e7c27b18bfaa
nlichodeevskiy <nicklihorus4@gmail.com>
2021-05-17 14:30:26 +0700

    refactoring Avatar

--- src/ui/content/Avatar/Avatar.tsx
+++ src/ui/content/Avatar/Avatar.tsx
@@ -1,37 +1,76 @@
 import React from 'react';
 import { useComponents } from '@steroidsjs/core/hooks';
-// import { Size } from './SizeContext'
+
+type AvatarSizes = 'large' | 'medium' | 'small';
 
 interface IAvatarProps {
-    avatarUrl?: string,
-    title?: string,
-    src?: React.ReactNode,
-    srcSet?: string;
+    /**
+     * Альтернативный текст для изображения
+     * @example {'default image'}
+     * @returns {string}
+     */
     alt?: string,
-    size?: Size,
-    shape?: 'circle' | 'square',
-    className?: CssClassName,
-    view?: any
+
+    /**
+     * Дополнительный CSS-класс
+     */
+    className?: CssClassName;
+
+    /**
+     * Дочерние элементы
+     */
     children?: React.ReactNode,
+
+    //TODO - Adaptive sizes { xs: number, sm: number, ...}
+    /**
+     * Размер аватара
+     * @example {'medium'}
+     * @returns {AvatarSizes | number}
+     */
+    size?: AvatarSizes | number,
+
+    /**
+     * Форма аватара
+     * @example {'circle'}
+     * @returns {'circle' | 'square' | string}
+     */
+    shape?: 'circle' | 'square' | string,
+
+    /**
+     * Ссылка на изображение для аватара
+     * @example {'https://user/avatar.png'}
+     * @returns {string}
+     */
+    src?: string,
+
+    // TODO Список адаптивных разрешений изображения
+    srcSet?: string;
+
+    /**
+     * Переопределение view React компонента для кастомизации отображения
+     * @example MyCustomView
+     */
+    view?: CustomView;
+
+    /**
+     * Объект CSS стилей
+     * @example {width: '30px'}
+     */
     style?: React.CSSProperties,
 }
 
-export interface IAvatarViewProps extends IAvatarProps {
-    
-}
+export type IAvatarViewProps = IAvatarProps
 
-export type Size = 'large' | 'medium' | 'small' | number;
-
-function Avatar (props: IAvatarProps) {
-    const components = useComponents()
+function Avatar(props: IAvatarProps) {
+    const components = useComponents();
     return components.ui.renderView(props.view || 'content.AvatarView', {
-        ...props
+        ...props,
     });
 }
 
 Avatar.defaultProps = {
     size: 'medium',
     shape: 'circle',
-}
+};
 
 export default Avatar;
---
+++ src/ui/content/index.ts
@@ -0,0 +1,17 @@
+import {Avatar, AvatarGroup} from './Avatar';
+import Card from './Card';
+import DropDown from './DropDown';
+
+export {
+    Avatar,
+    AvatarGroup,
+    Card,
+    DropDown,
+};
+
+export default {
+    Avatar,
+    AvatarGroup,
+    Card,
+    DropDown,
+};

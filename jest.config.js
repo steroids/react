1bc5e0bba3340345e657060e0c351ad40580fc24
Vladimir Kozhin <hello@kozhindev.com>
2021-06-27 12:18:19 +0700

    debug tests

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,7 @@
 const isLocal = require('fs').existsSync(require('path').resolve(__dirname, 'react-bootstrap'));
 
+console.log('Jest, isLocal: ' + isLocal);
+
 module.exports = {
     verbose: true,
     transform: {

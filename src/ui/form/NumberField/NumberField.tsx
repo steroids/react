0f4eb1aad22710db530f757d0e1290460816c512
AsgardPavlov <pasgard9@gmail.com>
2022-02-28 16:49:56 +0700

    fix NumberFieldView

--- src/ui/form/NumberField/NumberField.tsx
+++ src/ui/form/NumberField/NumberField.tsx
@@ -50,7 +50,7 @@ function NumberField(props: INumberFieldProps & IFieldWrapperOutputProps): JSX.E
     props.inputProps = useMemo(() => ({
         name: props.input.name,
         value: props.input.value ?? '',
-        onChange: e => props.input.onChange(e.target?.value || e.nativeEvent?.text),
+        onChange: value => props.input.onChange(value),
         type: 'number',
         min: props.min,
         max: props.max,

c05dfd79e068f85a44fe9e18e27db4e6eafb028f
dmartynov <denis.martynov.93@mail.ru>
2022-03-11 11:56:42 +0700

    v2.2.21 rewrite class to FC, fix form updating while editing FieldSet fields

--- src/ui/form/FieldSet/FieldSet.tsx
+++ src/ui/form/FieldSet/FieldSet.tsx
@@ -1,17 +1,16 @@
-import * as React from 'react';
+import React, {useContext, useMemo} from 'react';
 import _isString from 'lodash-es/isString';
-import {components} from '../../../hoc';
-import {IComponentsHocOutput} from '../../../hoc/components';
-import Field from '../Field';
-import {IFieldProps} from '../Field/Field';
-import {FormContext, IFormContext} from '../Form/Form';
-import {mergeLayoutProp} from '../../../utils/form';
+import {Field} from '@steroidsjs/core/ui/form';
+import {IFieldProps} from '@steroidsjs/core/ui/form/Field/Field';
+import {FormContext, IFormContext} from '@steroidsjs/core/ui/form/Form/Form';
+import {useComponents} from '@steroidsjs/core/hooks';
+import {mergeLayoutProp} from '@steroidsjs/core/utils/form';
 
 /**
  * FieldSet
  * Компонент для группировки полей формы
  */
-export interface IFieldSetProps extends IFormContext, IComponentsHocOutput {
+export interface IFieldSetProps extends IFormContext {
     /**
      * Конфигурационный массив с полями формы
      */
@@ -43,40 +42,41 @@ export interface IFieldSetViewProps {
     label?: string | any,
 }
 
-@components('ui')
-export default class FieldSet extends React.PureComponent<IFieldSetProps> {
-    render() {
-        const FieldSetView = this.props.view || this.props.ui.getView('form.FieldSetView');
-        return (
-            <FormContext.Consumer>
-                {context => (
-                    <FormContext.Provider
-                        value={{
-                            formId: this.props.formId || context.formId,
-                            model: this.props.model || context.model,
-                            prefix: [context.prefix, this.props.prefix]
-                                .filter(Boolean)
-                                .join('.'),
-                            layout: mergeLayoutProp(context.layout, this.props.layout),
-                        }}
-                    >
-                        <FieldSetView
-                            className={this.props.className}
-                            label={this.props.label}
-                            {...this.props}
-                        >
-                            {this.props.children}
-                            {(this.props.fields || []).map((field: any, index) => (
-                                <Field
-                                    key={index}
-                                    {...(_isString(field) ? {attribute: field} : field)}
-                                    prefix={null}
-                                />
-                            ))}
-                        </FieldSetView>
-                    </FormContext.Provider>
-                )}
-            </FormContext.Consumer>
-        );
-    }
+function FieldSet(props: IFieldSetProps): JSX.Element {
+    const components = useComponents();
+    const context = useContext(FormContext);
+
+    const contextValue = useMemo(() => ({
+        formId: props.formId || context.formId,
+        model: props.model || context.model,
+        prefix: [context.prefix, props.prefix]
+            .filter(Boolean)
+            .join('.'),
+        layout: mergeLayoutProp(context.layout, props.layout),
+        provider: context.provider,
+        reducer: context.reducer,
+    }), [context.provider, context.reducer, props.formId, props.layout, props.model, props.prefix]);
+
+    const FieldSetView = props.view || components.ui.getView('form.FieldSetView');
+
+    return (
+        <FormContext.Provider value={contextValue}>
+            <FieldSetView
+                className={props.className}
+                label={props.label}
+                {...props}
+            >
+                {props.children}
+                {(props.fields || []).map((field: any, index) => (
+                    <Field
+                        key={index}
+                        {...(_isString(field) ? {attribute: field} : field)}
+                        prefix={contextValue.prefix}
+                    />
+                ))}
+            </FieldSetView>
+        </FormContext.Provider>
+    );
 }
+
+export default FieldSet;

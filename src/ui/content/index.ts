93972e22e99b3f85af081454add097a431546bb2
AsgardPavlov <pasgard9@gmail.com>
2022-03-29 17:31:37 +0700

    v2.2.36 - fix Collapse, CollapseItem export

--- src/ui/content/index.ts
+++ src/ui/content/index.ts
@@ -1,11 +1,14 @@
 import {Avatar, AvatarGroup} from './Avatar';
+import {Collapse, CollapseItem} from './Collapse';
 import Card from './Card';
 import DropDown from './DropDown';
-import Alert from './Alert'
+import Alert from './Alert';
 
 export {
     Avatar,
     AvatarGroup,
+    Collapse,
+    CollapseItem,
     Alert,
     Card,
     DropDown,
@@ -14,8 +17,9 @@ export {
 export default {
     Avatar,
     AvatarGroup,
+    Collapse,
+    CollapseItem,
     Alert,
     Card,
     DropDown,
-    
 };

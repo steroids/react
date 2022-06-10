bd1d9fd1ce3d5a5eceb5d669f8cf3590cadb67b3
Vladislav <vladislav54415@gmail.com>
2022-06-10 12:37:10 +0700

    2.2.77 useFetch fix. Resest axiosError on refetch

--- src/hooks/useFetch.ts
+++ src/hooks/useFetch.ts
@@ -126,6 +126,7 @@ export default function useFetch(rawConfig: IFetchConfig = null): IFetchResult {
             });
         }
 
+        setAxiosError(null);
         setData(null);
 
         if (config) {

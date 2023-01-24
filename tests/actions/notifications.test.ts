79387aae94363433452d6f6036647cbecaffb095
RaayNoff <kraxtv471365@gmail.com>
2023-01-24 11:51:30 +0700

    Added showNotification tests

--- tests/actions/notifications.test.ts
+++ tests/actions/notifications.test.ts
@@ -47,23 +47,58 @@ describe('actions notifications', () => {
 
         //ToDO
         // - showNotification
-        // - closeNotification
         describe('showNotification', () => {
             //message = ... , level = null, params = def
 
             it('with default arguments', () => {
                 const message = 'You won 1000000 dollars';
-                const level = 'primary';
-                const id = 1;
+                const level = 'success';
+                const id = '1';
+                const position = 'top-right';
 
                 const expectedActions = [
                     {
                         type: NOTIFICATIONS_SHOW,
+                        id,
+                        message,
+                        level,
+                        position,
+                    },
+                ];
+
+                store.dispatch(showNotification(message));
+                expect(store.getActions()).toEqual(expectedActions);
+            });
+
+            // message = ... , level = ..., params = def
+
+            it('with level argument', () => {
+                const message = 'This button will blow up you computer ^_^';
+                const level = 'primary';
+                const id = '2';
+                const position = 'top-right';
+
+                const expectedActions = [
+                    {
+                        type: NOTIFICATIONS_SHOW,
+                        id,
+                        message,
+                        level,
+                        position,
                     },
                 ];
 
                 store.dispatch(showNotification(message, level));
+                expect(store.getActions()).toEqual(expectedActions);
             });
         });
+
+        //   describe('setFlashes', () => {
+        //       it('with correct data', () => {});
+        //   });
+        //   const flashes = {
+        //       key1: 'key1',
+        //       key2: 'key2',
+        //   };
     });
 });

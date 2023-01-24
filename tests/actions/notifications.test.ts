3ad69cdc2e1189189aba8084ee5461168ab4bc7b
RaayNoff <kraxtv471365@gmail.com>
2023-01-24 16:10:23 +0700

    Review code changes

--- tests/actions/notifications.test.ts
+++ tests/actions/notifications.test.ts
@@ -30,7 +30,6 @@ describe('actions notifications', () => {
             ];
 
             store.dispatch(closeNotification());
-
             expect(store.getActions()).toEqual(expectedActions);
         });
 
@@ -49,21 +48,16 @@ describe('actions notifications', () => {
         });
 
         describe('showNotification', () => {
-            //message = ... , level = null, params = def
-
             it('with default arguments', () => {
                 const message = 'You won 1000000 dollars';
-                const level = 'success';
-                const notificationId = '1';
-                const position = 'top-right';
 
                 const expectedActions = [
                     {
                         type: NOTIFICATIONS_SHOW,
-                        id: notificationId,
+                        id: '1',
                         message,
-                        level,
-                        position,
+                        level: 'success',
+                        position: 'top-right',
                     },
                 ];
 
@@ -71,21 +65,17 @@ describe('actions notifications', () => {
                 expect(store.getActions()).toEqual(expectedActions);
             });
 
-            // message = ... , level = ..., params = def
-
             it('with level argument', () => {
                 const message = 'This button will blow up you computer ^_^';
                 const level = 'primary';
-                const notificationId = '2';
-                const position = 'top-right';
 
                 const expectedActions = [
                     {
                         type: NOTIFICATIONS_SHOW,
-                        id: notificationId,
+                        id: '2',
                         message,
                         level,
-                        position,
+                        position: 'top-right',
                     },
                 ];
 
@@ -96,10 +86,7 @@ describe('actions notifications', () => {
 
         describe('setFlashes', () => {
             it('with filled flashes', () => {
-                const infoMessage = 'This bycycle is 20% off ';
-                const notificationId = '3';
-                const level = 'info';
-                const position = 'top-right';
+                const infoMessage = 'This bicycle is 20% off ';
 
                 const flashes = {
                     info: infoMessage,
@@ -108,10 +95,10 @@ describe('actions notifications', () => {
                 const expectedActions = [
                     {
                         type: NOTIFICATIONS_SHOW,
-                        id: notificationId,
+                        id: '3',
                         message: infoMessage,
-                        level,
-                        position,
+                        level: 'info',
+                        position: 'top-right',
                     },
                 ];
 
@@ -124,7 +111,6 @@ describe('actions notifications', () => {
                 const expectedActions = [];
 
                 store.dispatch(setFlashes(flashes));
-
                 expect(store.getActions()).toEqual(expectedActions);
             });
         });

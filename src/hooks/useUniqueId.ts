688391e4f95ab00041ee76db04aea8b8412d9002
RaayNoff <kraxtv471365@gmail.com>
2023-01-30 15:07:14 +0700

    Added useUniqueId test

--- src/hooks/useUniqueId.ts
+++ src/hooks/useUniqueId.ts
@@ -2,7 +2,7 @@ import {useRef} from 'react';
 import _uniqueId from 'lodash-es/uniqueId';
 
 export default function useUniqueId(prefix: string) {
-    const idRef = useRef();
+    const idRef = useRef<string>('');
     if (!idRef.current) {
         idRef.current = _uniqueId(prefix);
     }
---
+++ tests/hooks/useUniqueId.test.tsx
@@ -0,0 +1,31 @@
+import * as React from 'react';
+
+import _uniqueId from 'lodash-es/uniqueId';
+import useUniqueId from '../../src/hooks/useUniqueId';
+import mountWithApp from '../mountWithApp';
+
+const MockResultComponent = (props: any) => <div />;
+const MockComponent = (prefix: string) => (
+    <MockResultComponent uniqueId={useUniqueId(prefix)} />
+);
+
+jest.mock('lodash-es/uniqueId');
+
+describe('useUniqueId hook', () => {
+    it('default behavior', () => {
+        const prefix = 'usefulHook_';
+
+        const mockImplementationResult = `${prefix}1`;
+
+        const expectedResult = mockImplementationResult;
+
+        _uniqueId.mockImplementation(() => mockImplementationResult);
+
+        const wrapper = mountWithApp(MockComponent, prefix);
+
+        const uniqueId = wrapper.find('MockResultComponent').prop('uniqueId');
+
+        expect(uniqueId).toBe(expectedResult);
+        expect(typeof uniqueId).toBe('string');
+    });
+});

ecee79e58dec663686b9ce0f3bd38bc42cb0301c
Daria Kuzminykh <ds.kuzminyh@yandex.ru>
2023-03-09 19:03:34 +0800

    Added tests

---
+++ tests/ui/typography/Title/Title.test.tsx
@@ -0,0 +1,91 @@
+import '@testing-library/jest-dom';
+import {render} from '../../../customRender';
+import {Title} from '../../../../src/ui/typography';
+import TitleMockView from './TitleMockView';
+import {getElementByTag, JSXWrapper} from '../../../helpers';
+
+describe('Title tests', () => {
+    const requiredPropsForTest = {
+        testId: 'title-test',
+        view: TitleMockView,
+    };
+
+    const props = {
+        ...requiredPropsForTest,
+        type: 'h1',
+        content: 'Title test',
+        color: 'primary',
+        className: 'testClass',
+        style: {marginBottom: '70px'},
+    };
+
+    const expectedTitleClass = 'TitleView';
+    const wrapper = JSXWrapper(Title, props);
+
+    it('should be in the document and have classes', () => {
+        const {getByTestId} = render(wrapper);
+        const title = getByTestId(props.testId);
+
+        expect(title).toBeInTheDocument();
+        expect(title).toHaveClass(expectedTitleClass);
+        expect(title).toHaveClass(props.className);
+    });
+
+    it('should have right color, type', () => {
+        const {getByTestId} = render(wrapper);
+        const title = getByTestId(props.testId);
+
+        expect(title).toHaveClass(`${expectedTitleClass}_color_primary`);
+        expect(title).toHaveClass(`${expectedTitleClass}_type_h1`);
+    });
+
+    it('should have correct content', () => {
+        const {getByText} = render(wrapper);
+        const content = getByText(props.content);
+
+        expect(content).toBeInTheDocument();
+    });
+
+    it('should have correct html tag', () => {
+        const {container} = render(JSXWrapper(Title, props));
+        const expectedHtmlTag = 'h1';
+        const title = getElementByTag(container, expectedHtmlTag);
+        expect(title).toBeInTheDocument();
+    });
+
+    it('without some props, should use default html tag', () => {
+        const {container} = render(JSXWrapper(Title, requiredPropsForTest));
+        const expectedHtmlTag = 'h2';
+        const title = getElementByTag(container, expectedHtmlTag);
+        expect(title).toBeInTheDocument();
+    });
+
+    it('should have external html tag', () => {
+        const tag = 'h5';
+        const {container} = render(JSXWrapper(Title, {
+            ...props,
+            tag,
+        }));
+
+        const title = getElementByTag(container, tag);
+        expect(title).toBeInTheDocument();
+    });
+
+    it('should have children', () => {
+        const children = 'children';
+        const {getByTestId} = render(JSXWrapper(Title, {
+            ...props,
+            children,
+        }));
+
+        const title = getByTestId(props.testId);
+        expect(title).toHaveTextContent(children);
+    });
+
+    it('should have right style', () => {
+        const {getByTestId} = render(wrapper);
+        const title = getByTestId(props.testId);
+
+        expect(title).toHaveStyle(props.style);
+    });
+});
---
+++ tests/ui/typography/Title/TitleMockView.tsx
@@ -0,0 +1,43 @@
+import * as React from 'react';
+import {useBem} from '../../../../src/hooks';
+import {ITitleViewProps} from '../../../../src/ui/typography/Title/Title';
+
+interface ITitleMockViewProps extends ITitleViewProps {
+    testId: string,
+}
+
+const TYPE_MAPPING = {
+    h1: 'h1',
+    h2: 'h2',
+    h3: 'h3',
+    h4: 'h4',
+    h5: 'h5',
+    h6: 'h6',
+    subtitle: 'h6',
+};
+
+export default function TitleView(props: ITitleMockViewProps) {
+    const bem = useBem('TitleView');
+    const tag = props.tag || TYPE_MAPPING[props.type!] || 'h2';
+
+    return (
+        React.createElement(
+            tag,
+            {
+                className: bem(
+                    bem.block({
+                        type: props.type,
+                        color: props.color,
+                    }),
+                    props.className,
+                ),
+                style: props.style,
+                'data-testid': props.testId,
+            },
+            <>
+                {props.content}
+                {props.children}
+            </>,
+        )
+    );
+}

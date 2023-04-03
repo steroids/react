d1e9194f8b9b4db35401e3dd0cae104a1dda35d4
Dmitriy Babiy <dimascelecto@gmail.com>
2023-04-03 15:35:20 +0700

    Add tests Field

--- tests/ui/form/Field/Field.test.tsx
+++ tests/ui/form/Field/Field.test.tsx
@@ -1,21 +1,41 @@
 import '@testing-library/jest-dom';
+import {screen} from '@testing-library/dom';
 import {render} from '../../../customRender';
-import Field from '../../../../src/ui/form/Field/Field';
+import Field, {IFieldProps} from '../../../../src/ui/form/Field/Field';
 import {getElementByClassName, JSXWrapper} from '../../../helpers';
 
 describe('Field tests', () => {
     const props = {
-        testId: 'tooltip-test',
         attribute: 'field',
-        component: 'PasswordField',
-    };
+    } as IFieldProps;
 
-    const wrapper = JSXWrapper(Field, props);
+    it('should be in the document with default', () => {
+        const {container} = render(JSXWrapper(Field, props));
+        const field = getElementByClassName(container, 'InputFieldView');
 
-    it('should be in the document', () => {
-        const {getByTestId} = render(wrapper);
-        const tooltip = getByTestId(props.testId);
-
-        expect(tooltip).toBeInTheDocument();
+        expect(field).toBeInTheDocument();
     });
+
+    it('should be in the document with component', () => {
+        const component = 'PasswordField';
+        const {container} = render(JSXWrapper(Field, {
+            ...props,
+            component,
+        }));
+        const fieldComponent = getElementByClassName(container, 'PasswordFieldView');
+
+        expect(fieldComponent).toBeInTheDocument();
+    });
+
+    // it('should be in the document with model', () => {
+    //     const model = {attributes: [{attribute: 'category', field: 'PasswordField'}]};
+    //     const {container} = render(JSXWrapper(Field, {
+    //         ...props,
+    //         model,
+    //     }));
+    //     screen.debug();
+    //     const fieldModel = getElementByClassName(container, 'PasswordFieldView');
+
+    //     expect(fieldModel).toBeInTheDocument();
+    // });
 });

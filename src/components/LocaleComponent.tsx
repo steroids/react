671320212f414d9afd18f3eea5052db4d43a5d39
Dmitriy Babiy <dimascelecto@gmail.com>
2023-04-07 13:03:44 +0700

    Fixed component description

--- src/components/LocaleComponent.tsx
+++ src/components/LocaleComponent.tsx
@@ -9,7 +9,7 @@ import 'moment/locale/ru';
  * Locale Component
  * Компонент для локализации приложения. Поддерживает конфигурацию языка и временной зоны
  *
- * Пример строки: `{  ('{count} {count, plural, one{день} few{дня} many{дней}}', {count: 2})}`
+ * Пример строки: `{__('{count} {count, plural, one{день} few{дня} many{дней}}', {count: 2})}`
  */
 export default class LocaleComponent {
     backendTimeDiff: null;

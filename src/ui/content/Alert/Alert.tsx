f8de1696faeea631cdaf5cedf95e244e386c0847
Dmitriy <kraxtv471365@gmail.com>
2023-05-22 12:51:33 +0700

    Fixed pipeline

--- src/ui/content/Alert/Alert.tsx
+++ src/ui/content/Alert/Alert.tsx
@@ -11,7 +11,7 @@ export interface IAlertProps extends IUiComponent {
      * Типы Оповещений
      * @example 'info'
      */
-    type: 'success' | 'info' | 'warning' | 'error' | 'default' | string,
+    type?: 'success' | 'info' | 'warning' | 'error' | 'default' | string,
 
     /** Основное сообщения Оповещения
      * @example 'Sending is confirmed!'

f8f3b82daea39232f6b4a1787008393ef8a6ea82
Dmitriy <kraxtv471365@gmail.com>
2023-09-08 12:24:55 +0700

    Cursor bug

--- src/hooks/useSaveCursorPosition.tsx
+++ src/hooks/useSaveCursorPosition.tsx
@@ -1,22 +1,42 @@
+/* eslint-disable no-return-assign */
+/* eslint-disable no-unused-expressions */
 import React, {ChangeEvent} from 'react';
 import {IInputParams} from 'src/ui/form/Field/fieldWrapper';
+import _isNull from 'lodash-es/isNull';
 
-export default function useSaveCursorPosition(inputParams: IInputParams, inputRef = null) {
+export default function useSaveCursorPosition(
+    inputParams: IInputParams,
+    inputRef = null,
+    onSelectionStart?: VoidFunction,
+    onSelectionEnd?: VoidFunction,
+    validators?: ((value: string | number | boolean) => boolean)[],
+) {
     const [cursor, setCursor] = React.useState(null);
     const _inputRef = React.useRef(null);
-    const currentInputRef = inputRef || _inputRef;
+    const currentInputRef: React.MutableRefObject<HTMLInputElement> = inputRef || _inputRef;
 
     React.useEffect(() => {
         const inputElement: HTMLInputElement = currentInputRef.current;
         if (inputElement) {
+            if (onSelectionStart) {
+                onSelectionStart();
+            }
+
             inputElement.setSelectionRange(cursor, cursor);
+
+            if (onSelectionEnd) {
+                onSelectionEnd();
+            }
         }
-    }, [currentInputRef, cursor, inputParams.value]);
+    }, [currentInputRef, cursor, inputParams.value, onSelectionEnd, onSelectionStart]);
 
     const onChange = React.useCallback((event: ChangeEvent<HTMLInputElement>, value = null) => {
-        setCursor(event.target.selectionStart);
-        inputParams.onChange(value || event.target.value);
-    }, [inputParams]);
+        setCursor(event?.target.selectionStart);
+
+        if (!validators || validators.every(validator => validator(value || event.target.value))) {
+            inputParams.onChange(value || event.target.value);
+        }
+    }, [inputParams, validators]);
 
     return {
         inputRef: currentInputRef,
--- src/ui/form/NumberField/NumberField.ts
+++ src/ui/form/NumberField/NumberField.ts
@@ -1,7 +1,10 @@
-import {ChangeEvent, useMemo} from 'react';
+/* eslint-disable max-len */
+import React, {ChangeEvent, useMemo, useCallback, useRef} from 'react';
 import {IBaseFieldProps} from '../InputField/InputField';
-import {useComponents} from '../../../hooks';
+import {useComponents, useSaveCursorPosition} from '../../../hooks';
 import fieldWrapper, {IFieldWrapperOutputProps} from '../Field/fieldWrapper';
+
+const DEFAULT_STEP = 1;
 
 /**
  * NumberField
@@ -40,25 +43,87 @@ export interface INumberFieldViewProps extends INumberFieldProps, IFieldWrapperO
         max: number,
         step: string | number,
     },
+    inputRef: React.MutableRefObject<any>,
+    onStepUp: VoidFunction,
+    onStepDown: VoidFunction,
+    onKeyDown: VoidFunction,
 }
 
 function NumberField(props: INumberFieldProps & IFieldWrapperOutputProps): JSX.Element {
     const components = useComponents();
 
-    props.inputProps = useMemo(() => ({
+    const validateNumericInput = (value) => {
+        if (value === '' || value === null || value === undefined) {
+            return true;
+        }
+
+        const regex = /^-?\d*\.?\d+$/;
+        return regex.test(value);
+    };
+
+    const {inputRef: currentInputRef, onChange} = useSaveCursorPosition(
+        props.input,
+        null,
+        null,
+        null,
+        [validateNumericInput],
+    );
+
+    const setValidity = React.useCallback((message: string) => {
+        currentInputRef.current?.setCustomValidity(message);
+    }, [currentInputRef]);
+
+    React.useEffect(() => {
+        const defaultValidity = `The number is not included in the range ${props.min} to ${props.max}`;
+
+        // eslint-disable-next-line no-unused-expressions
+        props.input.value > props.max
+            || props.input.value < props.min
+            || props.input.value === ''
+            || props.input.value === null
+            || props.input.value === undefined
+            ? setValidity(__(defaultValidity))
+            : setValidity('');
+    }, [currentInputRef, props.input.value, props.max, props.min, setValidity]);
+
+    const onStepUp = useCallback(() => {
+        onChange(null, String(Number(currentInputRef?.current?.value) + Number(props.step || DEFAULT_STEP)));
+    }, [currentInputRef, onChange, props.step]);
+
+    const onStepDown = useCallback(() => {
+        onChange(null, String(Number(currentInputRef?.current?.value) - Number(props.step || DEFAULT_STEP)));
+    }, [currentInputRef, onChange, props.step]);
+
+    const onKeyDown = useCallback((event: KeyboardEvent) => {
+        if (event.key === 'ArrowUp') {
+            onStepUp();
+        } else if (event.key === 'ArrowDown') {
+            onStepDown();
+        }
+    }, [onStepDown, onStepUp]);
+
+    const inputProps = useMemo(() => ({
         name: props.input.name,
         value: props.input.value ?? '',
-        onChange: value => props.input.onChange(value),
-        type: 'number',
+        onChange,
+        type: 'text',
         min: props.min,
         max: props.max,
         step: props.step,
         placeholder: props.placeholder,
         disabled: props.disabled,
+        autoComplete: 'off',
+        onKeyDown,
         ...props.inputProps,
-    }), [props.disabled, props.input, props.inputProps, props.placeholder, props.min, props.max, props.step]);
+    }), [props.input.name, props.input.value, props.min, props.max, props.step, props.placeholder, props.disabled, props.inputProps, onChange, onKeyDown]);
 
-    return components.ui.renderView(props.view || 'form.NumberFieldView', props);
+    return components.ui.renderView(props.view || 'form.NumberFieldView', {
+        ...props,
+        inputProps,
+        onStepUp,
+        onStepDown,
+        inputRef: currentInputRef,
+    });
 }
 
 NumberField.defaultProps = {

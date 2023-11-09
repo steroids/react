058c4a5bb7224f27d867bb28994ecaebfaa590ef
Vladislav Koktashev <vladislav54415@gmail.com>
2023-11-09 11:49:43 +0300

    render children when parent component is missing

--- src/ui/nav/Router/Router.tsx
+++ src/ui/nav/Router/Router.tsx
@@ -373,6 +373,9 @@ function Router(props: IRouterProps): JSX.Element {
 
         const result = renderComponent(routeItem, activePath, {...routeProps, children});
         if (!result) {
+            if (children) {
+                return children;
+            }
             // eslint-disable-next-line no-console
             console.error('Not found component for route:', routeItem);
         }

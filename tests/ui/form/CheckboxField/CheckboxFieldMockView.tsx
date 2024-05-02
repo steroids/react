9f6dde8a7906617fdf00afaaddf1f66c5db9cee4
Konstantin Smirnov <smirnov.konstantiin@gmail.com>
2024-05-02 09:53:10 +0700

    Update CheckboxFieldMockView

--- tests/ui/form/CheckboxField/CheckboxFieldMockView.tsx
+++ tests/ui/form/CheckboxField/CheckboxFieldMockView.tsx
@@ -6,34 +6,45 @@ export default function CheckboxFieldView(props: ICheckboxFieldViewProps) {
     const bem = useBem('CheckboxFieldView');
     const id = useUniqueId('checkbox');
 
+    const customVariableColorStyle = {'--checkbox-custom-color': props.color} as React.CSSProperties;
+
     return (
         <div
-            className={
-                `${bem.block()} 
-                 ${bem(props.className)}
-                 ${props.errors ? 'has-errors' : ''}`
-            }
-            style={props.style}
+            className={bem(
+                bem.block({
+                    size: props.size,
+                    hasErrors: !!props.errors,
+                }),
+                props.className,
+            )}
+            style={{
+                ...props.style,
+                ...customVariableColorStyle,
+            }}
+            onClick={props.onChange}
+            role='button'
+            aria-hidden
         >
             <input
-                className={bem(
-                    bem.element('input'),
-                )}
-                id={id}
-                {...props.inputProps}
+                className={bem.element('input', {
+                    hasCustomColor: !!props.color,
+                })}
+                id={props.id || id}
                 disabled={props.disabled}
                 required={props.required}
-                type="checkbox"
+                {...props.inputProps}
             />
             <label
-                className={bem(
-                    bem.element('label'),
-                )}
-                htmlFor={id}
+                className={bem.element('label', {
+                    'has-label-only': !props.id,
+                })}
+                htmlFor={props.id || id}
             >
-                <span className={bem.element('label-text', {required: props.required})}>
-                    {props.label}
-                </span>
+                {props.label && (
+                    <span className={bem.element('label-text', {required: props.required})}>
+                        {props.label}
+                    </span>
+                )}
             </label>
         </div>
     );

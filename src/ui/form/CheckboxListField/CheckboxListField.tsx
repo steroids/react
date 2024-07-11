d4348ab6b1880bfa2bcfb67e138c41a6ecf406c4
Konstantin Smirnov <smirnov.konstantiin@gmail.com>
2024-07-11 17:34:58 +0700

    Revert selectAll from useDataSelect

--- src/ui/form/CheckboxListField/CheckboxListField.tsx
+++ src/ui/form/CheckboxListField/CheckboxListField.tsx
@@ -120,7 +120,6 @@ function CheckboxListField(props: ICheckboxListFieldProps): JSX.Element {
         multiple: props.multiple,
         primaryKey: props.primaryKey,
         selectFirst: props.selectFirst,
-        selectAll: props.selectAll,
         items,
         inputValue: props.input.value,
     });
--- src/ui/form/DropDownField/DropDownField.tsx
+++ src/ui/form/DropDownField/DropDownField.tsx
@@ -156,16 +156,6 @@ export interface IDropDownFieldProps extends IFieldWrapperInputProps,
     },
 
     /**
-     * При выборе всех элементов отображать label из itemToSelectAll, работает только при multiple: true
-     * @example
-     * {
-     *  label: 'All',
-     *  id: 'all'
-     * }
-     */
-    showSelectAllLabel?: boolean,
-
-    /**
      * Кастомная вьюшка для элемента
      */
     itemView?: CustomView,
@@ -283,7 +273,6 @@ function DropDownField(props: IDropDownFieldProps & IFieldWrapperOutputProps): J
     } = useDataSelect({
         multiple: props.multiple,
         selectFirst: props.selectFirst,
-        selectAll: props.selectAll,
         selectedIds: inputSelectedIds,
         primaryKey: props.primaryKey,
         groupAttribute: props.groupAttribute,
@@ -427,8 +416,6 @@ function DropDownField(props: IDropDownFieldProps & IFieldWrapperOutputProps): J
         renderItem,
         onItemRemove,
         hasGroup,
-        isSelectedAll,
-        showSelectAllLabel: props.showSelectAllLabel,
         multiple: props.multiple,
         isSearchAutoFocus: props.isSearchAutoFocus,
         itemToSelectAll: normalizedItemToSelectAll,
@@ -442,8 +429,8 @@ function DropDownField(props: IDropDownFieldProps & IFieldWrapperOutputProps): J
         showEllipses: props.showEllipses,
         errors: props.errors,
         disabled: props.disabled,
-    }), [isAutoComplete, items, hoveredId, selectedIds, searchInputProps, isOpened, isLoading, onOpen, selectedItems,
-        onReset, onClose, renderItem, onItemRemove, hasGroup, isSelectedAll, props.showSelectAllLabel, props.multiple, props.isSearchAutoFocus,
+    }), [isAutoComplete, items, hoveredId, selectedIds, searchInputProps, isOpened, isLoading, onOpen,
+        onReset, onClose, renderItem, onItemRemove, hasGroup, props.multiple, props.isSearchAutoFocus,
         props.className, props.style, props.size, props.color, props.outline, props.placeholder, props.showReset, props.showEllipses,
         props.errors, props.disabled, normalizedItemToSelectAll]);
 

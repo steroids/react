b15d51d0ae0c18fa46318a1683438443639cb296
arkady <arkadiy.korsakov.98@mail.ru>
2026-01-16 10:53:34 +0700

    steroids/dev#737 add attributes `action` and `itemsKey` for `TreeTable`

--- src/hooks/useList.tsx
+++ src/hooks/useList.tsx
@@ -578,10 +578,12 @@ export default function useList(config: IListConfig): IListOutput {
 
     // Check change items
     useUpdateEffect(() => {
-        dispatch([
-            listSetItems(config.listId, config.items),
-        ]);
-    }, [dispatch, config.items, config.listId]);
+        if (config.items) {
+            dispatch([
+                listSetItems(config.listId, config.items),
+            ]);
+        }
+    }, [dispatch, config.items, config.listId, list?.isFetched]);
 
     // Check change action
     const prevAction = usePrevious(config.action);

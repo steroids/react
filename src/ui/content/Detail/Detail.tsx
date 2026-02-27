5e123dfa063ef56fa2f82c12203be012b0aa0c89
arkady <arkadiy.korsakov.98@mail.ru>
2026-02-27 14:13:14 +0700

    steroids/dev#903 unify lodash usage across project

--- src/ui/content/Detail/Detail.tsx
+++ src/ui/content/Detail/Detail.tsx
@@ -1,6 +1,6 @@
 import React, {useRef, useMemo, useState, useEffect} from 'react';
-import _isArray from 'lodash/isArray';
-import _orderBy from 'lodash/orderBy';
+import _isArray from 'lodash-es/isArray';
+import _orderBy from 'lodash-es/orderBy';
 import {useComponents} from '../../../hooks';
 import DetailItem, {IDetailItemProps} from './DetailItem';
 import {IControlItem} from '../../nav/Controls/Controls';
